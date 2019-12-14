b5b0bc86eb48119d8f6485bb5ce59f21b8179158
MaxLucio528 <maxlucio_528@hotmail.com>
2019-12-14 16:53:35 -0300

    Version 1.1 added

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # JogoDaVelhaJS
-Site com o famoso Jogo da Velha ou Jogo do Galo em (PT-PT), produzido em JavaScript.
 
-Criado por: Max Lucio Martins de Assis
+Made by: Max Lucio Martins de Assis.
+
+This project is a simple website that has the famous "Jogo da Velha" implemented with JavaScript, it comes with a Dark Mode as well to play at night. It's compatible with (probably) any web browser.
+
+Current Version: 1.1
+
+## Changelog
+
+**Version 1.1 (14/12/2019) [FORMAT (DD/MM/YY)]**
+
+-> The website is in english as well as the comments.
+
+**Version 1.0 (12/09/2019) [FORMAT (DD/MM/YY)]**
+
+-> Initial version of the website, all the functions were implemented, but the website is in brazilian portuguese as well as the comments.
--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,44 @@
 /*
-    FT - FACULDADE DE TECNOLOGIA DA UNICAMP
+ *  JogoDaVelhaJS
+ *  CONTRIBUITORS:
+ *  -> Max Lucio Martins de Assis (GitHub - MaxLucio528)
+ * 
+ *  PURPOSE: This project is a simple website that has the famous "Jogo da Velha" implemented
+ *  with JavaScript, it comes with a Dark Mode as well to play at night. It's compatible with
+ *  (probably) any web browser.
+ */
 
-    Nome: Max Lucio Martins de Assis
-
-    Site: Jogo da Velha com JavaScript
-*/
-
-// Função que troca o CSS da página para um Modo Escuro ou Claro
+// This function switch between the Dark and Light Mode on the page.
 function modoTela(nomeId){
-    /*  
-        É verificado o modo da página a partir do texto exibido para o usuário na página. 
-        Se o texto estiver Modo Escuro, a página é trocada para o modo escuro e o texto é
-        trocado para Modo Claro.
-        Se o texto estiver Modo Claro, a página é trocada para o modo claro e o texto é
-        trocado para Modo Escuro. 
-    */
-    if(document.getElementById(nomeId).innerHTML === "Modo Escuro " + String.fromCodePoint(0x263E)){
+    /*
+     *  The mode of the page is verified from the text exhibited to the user on the page.
+     *  
+     *  If the text equals Dark Mode, the page is swithed to the Dark Mode and the text is
+     *  switched to Light Mode.
+     * 
+     *  If the text equals Light Mode, the page is switched to Light Mode and the text is
+     *  switched to Dark Mode.
+     */
+    if(document.getElementById(nomeId).innerHTML === "Dark Mode " + String.fromCodePoint(0x263E)){
         document.getElementById("stylesheet").href = "stylesheets/dark.css";
-        document.getElementById(nomeId).innerHTML = "Modo Claro " + String.fromCodePoint(0x2609);
+        document.getElementById(nomeId).innerHTML = "Light Mode " + String.fromCodePoint(0x2609);
     }else{
         document.getElementById("stylesheet").href = "stylesheets/light.css";
-        document.getElementById(nomeId).innerHTML = "Modo Escuro " + String.fromCodePoint(0x263E);
+        document.getElementById(nomeId).innerHTML = "Dark Mode " + String.fromCodePoint(0x263E);
     }
 }
 
-// Variáveis globais que indicam de quem é a vez no jogo e o vencedor.
+// Indicate who's turn is in the game and the winner respectively.
 var vez = 1;
 var vencedor = "";
 
-// Função que verificará se três casas são iguais ou não, indicando se há ou não um vencedor.
+// Verifies if three houses on the board are equal or not, indicating if there's a winner or not.
 function casasIguais(a, b, c){
     if((a == b) && (b == c) && (a != "")){
         if(a == "X")
-            vencedor = "Jogador 1";
+            vencedor = "Player 1";
         else
-            vencedor = "Jogador 2";
+            vencedor = "Player 2";
 
         return true;
     }else{
@@ -42,92 +46,90 @@ function casasIguais(a, b, c){
     }
 }
 
-// Função que verifica o estado atual do jogo.
+// Verifies the actual state of the game.
 function statusJogo(){
-    // Este array deixa salvo o estado de todas as casas no momento em que esta função foi chamada.
+    // Saves the state of all the board houses in the moment this funcion was called.
     var casas = new Array(9);
 
-    // Laço que salva as casas no array
+    // This loop saves the board houses in the array.
     for(var i = 0; i < 9; i++){
         casas[i] = document.getElementById("casa" + (i+1)).innerHTML;
     }
 
-    /*  
-        Este if verifica se alguma dessas funções retorna true, se isso ocorrer
-        há um vencedor para o jogo, é mandada para a função casasIguais o que consta
-        em três casas que se iguais ocasionariam na vitória de um jogador.
-    */
+    /*
+     *  This if summons the function "casasIguais" is all possible situations where a winner can
+     *  appear, if any of those conditions are met, there's a winner.
+     */
     if( casasIguais(casas[0], casas[1], casas[2]) || casasIguais(casas[3], casas[4], casas[5]) || 
         casasIguais(casas[6], casas[7], casas[8]) || casasIguais(casas[0], casas[3], casas[6]) || 
         casasIguais(casas[1], casas[4], casas[7]) || casasIguais(casas[2], casas[5], casas[8]) ||
         casasIguais(casas[0], casas[4], casas[8]) || casasIguais(casas[2], casas[4], casas[6])
     ){
         /*
-            Aqui é trocado o conteúdo do parágrafo de status do jogo para indicar quem venceu
-            e também, o botão de jogar volta a aparecer e exibe a opção de jogar novamente. 
-        */
-        document.getElementById("status").innerHTML = "O " + vencedor + " venceu!";
-        document.getElementById("play").innerHTML = "Jogar Novamente";
+         *  The content of the paragraph of the game status is switched to indicate who win
+         *  and, the play button reappears and shows the option to play again.
+         */
+        document.getElementById("status").innerHTML = "The " + vencedor + " won!";
+        document.getElementById("play").innerHTML = "Play Again";
         document.getElementById("play").disabled = false;
         document.getElementById("play").style.display = "block";
     }
 
-    /* 
-        Condição que verifica se houve empate, se isso ocorrer ele troca o conteúdo do
-        parágrafo de ID status e também volta a mostrar o botão que agora mostra a opção
-        de jogar novamente, que atualiza a página.
-    */
+    /*
+     *  Switches the content of the paragraph of the game status to indicate that a draw
+     *  happened and, the play button reappears and shows the option to play again.
+     */
     if(casas.indexOf("") === -1){
-        document.getElementById("status").innerHTML = "O jogo empatou!";
-        document.getElementById("play").innerHTML = "Jogar Novamente";
+        document.getElementById("status").innerHTML = "It's a draw!";
+        document.getElementById("play").innerHTML = "Play Again";
         document.getElementById("play").disabled = false;
         document.getElementById("play").style.display = "block";
     }
 }
 
-// Função que libera o início do jogo
+// Unlock the game start.
 function start(){
-    // Condição que reinicia a página para jogar novamente após o fim do jogo
-    if(document.getElementById("play").innerHTML === "Jogar Novamente")
+    // Refresh the page to play again after the game is finished.
+    if(document.getElementById("play").innerHTML === "Play Again")
         location.reload();
 
     /*
-        Aqui o botão é desabilitado e ele desaparece, para então ser exibida
-        na página que é a vez do primeiro jogador.
-    */
+     *  Here the button is disabled and he disappear, to then show on the
+     *  page that it's the first player turn.
+     */
     document.getElementById("play").disabled = true;
     document.getElementById("play").style.display = "none";
-    document.getElementById("status").innerHTML = "É a vez do jogador " + vez + "!"
+    document.getElementById("status").innerHTML = "It's " + vez + " turn!"
 }
 
-// Função que inicia o processo para marcar o X ou O no tabuleiro
+// Start the process to mark X or O in the board.
 function jogar(idCasa){
-    // Se o botão de jogar estiver desativado, ele libera o tabuleiro
+    // If the button to play is disabled, he unlocks the board.
     if(document.getElementById("play").disabled === true){
-        // Verificando se a casa clicada está vazia para preenchê-la
+        // Checking if the board house clicked is empty to fill it.
         if(document.getElementById(idCasa).innerHTML == ""){
-            // Verificando de quem é a vez quando a casa foi clicada
+            // Checking who's turn is when the board house was clicked.
             if(vez === 1){
-                // Ajustando a casa clicada para manter-se no padrão de estilo
+                // Adjusting the clicked house to keep the symmetry.
                 document.getElementById(idCasa).style.padding = "30px";
                 document.getElementById(idCasa).style.paddingTop = "22px";
                 document.getElementById(idCasa).style.paddingBottom = "22px";
-                // Passando a vez para o próximo jogador
+                // Passing the turn to the next player.
                 vez = 2;
-                document.getElementById("status").innerHTML = "É a vez do jogador " + vez + "!";
+                document.getElementById("status").innerHTML = "It's " + vez + " turn!";
                 document.getElementById(idCasa).innerHTML = "X";
             }else{
-                // Ajustando a casa clicada para manter-se no padrão de estilo
+                // Adjusting the clicked house to keep the symmetry.
                 document.getElementById(idCasa).style.padding = "30px";
                 document.getElementById(idCasa).style.paddingTop = "22px";
                 document.getElementById(idCasa).style.paddingBottom = "22px";
-                // Passando a vez para o próximo jogador
+                // Passing the turn to the next player.
                 vez = 1;
-                document.getElementById("status").innerHTML = "É a vez do jogador " + vez + "!";
+                document.getElementById("status").innerHTML = "It's " + vez + " turn!";
                 document.getElementById(idCasa).innerHTML = "O";
             }
-    
-            // Chamando a função que verifica o status do jogo, vendo se alguém ganhou.
+            
+            // Calling the function that verifies the game status, to see if someone won.
             statusJogo();
         }
     }
